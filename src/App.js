e568947ac9a5f0dd1c014972552c40855f618699
kemalozer23 <89297908+kemalozer23@users.noreply.github.com>
2023-04-04 01:16:00 +0300

    added subtype and type select

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import Header from './components/Header'
 import Body from './components/Body'
 import Pagination from './components/Pagination';
 
+import { Select, Option } from "@material-tailwind/react";
+
 
 
 function App() {
 
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
+
+  const [superType, setSuperType] = useState('Pokemon');
+  const [type, setType] = useState('Water');
 
   const nextPage = (page) => {
     setPage(page + 1);
@@ -22,24 +27,52 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get(`https://api.pokemontcg.io/v2/cards?supertype=Pokémon&page=${page}&pageSize=9&orderBy=name`);
+      const url = `https://api.pokemontcg.io/v2/cards?page=${page}&pageSize=9&orderBy=name&q=supertype:${superType} types:${type}`
+      console.log(url);
+      const result = await axios.get(url).catch(error => console.error(error));
       setData(result.data.data)
     };
 
     fetchData();
-  }, [page]);
+  }, [page, superType, type]);
 
   if (!data) return null;
 
   return (
     <>
       <Header />
+
+      <div className="w-1/4">
+        <Select label="Supertype" value={superType} onChange={(value) => setSuperType(value)}>
+          <Option value='Energy'>Energy</Option>
+          <Option value='Pokemon'>Pokemon</Option>
+          <Option value='Trainer'>Trainer</Option>
+        </Select>
+      </div>
+
+      <div className="w-1/4 pt-5">
+        <Select label="Type" value={type} onChange={(value) => setType(value)}>
+          <Option value='Colorless'>Colorless</Option>
+          <Option value='Darkness'>Darkness</Option>
+          <Option value='Dragon'>Dragon</Option>
+          <Option value='Fairy'>Fairy</Option>
+          <Option value='Fighting'>Fighting</Option>
+          <Option value='Fire'>Fire</Option>
+          <Option value='Grass'>Grass</Option>
+          <Option value='Lightning'>Lightning</Option>
+          <Option value='Metal'>Metal</Option>
+          <Option value='Psychic'>Psychic</Option>
+          <Option value='Water'>Water</Option>
+        </Select>
+      </div>
+
+      <Body data={data} />
+
       <Pagination 
         nextPage={nextPage}
         previousPage={previousPage}
         page={page}
       />
-      <Body data={data} />
     </>
   )
 }
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 
 const Body = ({data}) => {    
   return (
-    <div className='container mx-auto px-4 flex justify-center'>
+    <div className='container mx-auto px-4'>
         <div className="grid grid-cols-3 gap-9">
             {data.map(item => (
                 <Card key={item.id} src={item.images.small} alt={item.name} />
